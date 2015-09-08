7a5a27f50e72ac9989f10d1b0d56b52fc631351f
chunqishi <shicq@cs.brandeis.edu>
2015-09-09 04:47:14 +0800

    finish daemon

---
+++ alert_daemon.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# file        : alert_daemon.sh
+# description : daemon process manager 
+# creator     : Chunqi
+# version     : 0.1
+
+APP_DIR="$(dirname "$0")"
+APP_NAME="lapps_alert_daemon"
+PIDFILE="$APP_NAME.pid"
+LOGFILE="$APP_NAME.log"
+
+case "$1" in
+    start)
+        echo "Starting $APP_NAME ..."
+        PID=$($APP_DIR/email_ping.sh > $LOGFILE 2>&1 & echo $!)
+        if [ -z $PID ]; then
+                echo "Enable to start $APP_NAME. No PID was created"
+        else
+                echo $PID > $PIDFILE
+                echo "$APP_NAME started"
+        fi
+        ;;
+
+   status)
+        if [ -f $PIDFILE ]; then
+                PID=$(cat $PIDFILE)
+                if [ -z "$(ps aux | grep $PID | grep -v grep)" ]; then
+                        echo "$APP_NAME is dead but pidfile exists"
+                else
+                        echo "$APP_NAME is running"
+                fi
+        else
+                echo "$APP_NAME is not running"
+        fi
+        ;;
+
+    stop)
+        echo "Stopping $APP_NAME ..."
+        PID=$(cat $PIDFILE)
+        if [ -f $PIDFILE ]; then
+                kill -9 $PID
+                echo "$APP_NAME is stopping"
+                rm -f $PIDFILE
+        else
+                echo "$PIDFILE not found"
+        fi
+        ;;
+
+    restart)
+        $0 stop
+        $0 start
+        ;;
+
+    *)
+        echo "Usage $0 {status|start|stop|restart}"
+        exit 1
+        ;;
+esac
+
+exit 0
--- email_ping.sh
+++ email_ping.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+# file        : email_ping.sh
+# description : email alert if url broken 
+# creator     : Chunqi
+# version     : 0.0.1
+
 # Alert Daemon for Lapps Service Checking
 
 
@@ -5,23 +12,32 @@ email_ping ()     {
     local url=$1
     local subject=$2
     local email=$3
-    curl $url -s -f -o /dev/null || echo "$subject ($url)" | mail -s "$subject ($url)" "$email"
+    local httpcode=$(curl -o /dev/null --silent --head --write-out '%{http_code}' $url)
+    curl $url -s -f -o /dev/null || echo "$subject ($url) [httpcode=$httpcode]" | mail -s "$subject ($url) [httpcode=$httpcode]" "$email"
 } # email_ping
 
+while true; do
+    HOUR=`date +%H`
+    while [ $HOUR -ne "00" ]; do 
 
-# ServiceManager (/local/eldrad/users/lapps/tomcat7/8080)
-email_ping http://eldrad.cs-i.brandeis.edu:8080/ "Lapps ServiceManager" "shicq@cs.brandeis.edu, marc@cs.brandeis.edu"
+        # ServiceManager (/local/eldrad/users/lapps/tomcat7/8080)
+        email_ping http://eldrad.cs-i.brandeis.edu:8080/ "Lapps ServiceManager" "shicq@cs.brandeis.edu, marc@cs.brandeis.edu"
 
-# Lapps Service (/local/eldrad/users/lapps/tomcat7/8282)
-email_ping http://eldrad.cs-i.brandeis.edu:8282/ "Lapps Services" "shicq@cs.brandeis.edu, marc@cs.brandeis.edu"
-
-
-# Visualization Tool (/local/eldrad/users/lapps/tomcat7/8484)
-email_ping http://eldrad.cs-i.brandeis.edu:8484/ "Lapps Visualization Page" "shicq@cs.brandeis.edu, marc@cs.brandeis.edu"
+        # Lapps Service (/local/eldrad/users/lapps/tomcat7/8282)
+        email_ping http://eldrad.cs-i.brandeis.edu:8282/ "Lapps Services" "shicq@cs.brandeis.edu, marc@cs.brandeis.edu"
 
 
-# Galaxy (/local/eldrad/users/lapps/tomcat7/8000)
-email_ping http://eldrad.cs-i.brandeis.edu:8000/ "Galaxy" "shicq@cs.brandeis.edu, marc@cs.brandeis.edu"
+        # Visualization Tool (/local/eldrad/users/lapps/tomcat7/8484)
+        email_ping http://eldrad.cs-i.brandeis.edu:8484/ "Lapps Visualization Page" "shicq@cs.brandeis.edu, marc@cs.brandeis.edu"
+
+
+        # Galaxy (/local/eldrad/users/lapps/tomcat7/8000)
+        email_ping http://eldrad.cs-i.brandeis.edu:8000/ "Galaxy" "shicq@cs.brandeis.edu, marc@cs.brandeis.edu"
+
+        sleep 3600
+        HOUR=`date +%H`
+        done
+done
 
 # TEST1: curl www.websiteToTest.com -s -f -o /dev/null || echo "Website Down." | mail -s "Website is down" shicq@cs.brandeis.edu 
 
